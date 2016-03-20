279f8048bfb666604094aa74ceca1c5145814569
tuber <btu@thoughtworks.com>
2016-03-20 22:23:00 +0800

    [tuber] update change display mode

--- front-end/src/js/controllers/photo-wall-controller.js
+++ front-end/src/js/controllers/photo-wall-controller.js
@@ -1,2 +1,12 @@
 photosApp.controller('photoWallController', function () {
+  var vm = this;
+  const MODE_SWITCH = ['thumbnail', 'list'];
+  var current = 0;
+  vm.mode = MODE_SWITCH[current];
+  vm.button = './images/' + vm.mode + '.png';
+  vm.changeMode = function () {
+    current ^= 1;
+    vm.mode = MODE_SWITCH[current];
+    vm.button = './images/' + vm.mode + '.png';
+  }
 });
